cbe3d5f4e61f0fa2635385acc69b270d8469d8dc
Nathan Bubna <nathan@esha.com>
2013-11-01 09:50:36 -0700

    more informative log when polyfilling bind()

--- root/test/phantomShim.js
+++ root/test/phantomShim.js
@@ -1,7 +1,7 @@
 // PhantomJS doesn't support bind yet - https://github.com/ariya/phantomjs/issues/10522
-if (typeof Function.prototype.bind != 'function') {
+if (typeof Function.prototype.bind !== 'function') {
   if (console) {
-    console.warn('Using shim for Function.prototype.bind!');
+    console.log('\nTests are using a shim for Function.prototype.bind to polyfill testing for PhantomJS.\nIf you are running these tests in a browser, your results may be inaccurate.');
   }
   Function.prototype.bind = function() {
     var slice = Array.prototype.slice,
